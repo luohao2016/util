6c56dac879f3eda7290e14dc8bffbbe19dfa8ecb
jack.luo <luohao518@163.com>
2017-09-14 21:25:07 +0800

    fix

--- logger.js
+++ logger.js
@@ -2,11 +2,11 @@
 
 var log4js = require('log4js');
 log4js.configure({
-    appenders: { app: { type: 'file', filename: 'logs/server.log' } },
-    categories: { default: { appenders: ['app'], level: 'debug' } }
-  });
+  appenders: { app: { type: 'file', filename: 'logs/server.log' } },
+  categories: { default: { appenders: ['app'], level: 'debug' } }
+});
 
-exports.logger=function(name){
+exports.logger = function (name) {
   var logger = log4js.getLogger(name);
   return logger;
 }
